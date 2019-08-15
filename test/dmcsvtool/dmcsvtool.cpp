69c7eaa0d4f2731535d89053d270cae1e45d6895
brinkqiang <brink.qiang@gmail.com>
2019-08-15 14:04:06 +0800

    add

--- test/dmcsvtool/dmcsvtool.cpp
+++ test/dmcsvtool/dmcsvtool.cpp
@@ -7,25 +7,31 @@
 
 #include <vector>
 #include <queue>
+#include <filesystem>
 
 int main( int argc, char** argv )
 {
     std::string strFile = DMGetRootPath() + PATH_DELIMITER_STR + "slist.csv";
+
+
+    if (!std::filesystem::exists(strFile))
+    {
+        std::cout << "file: " << strFile << " not exist" << std::endl;
+        return -1;
+    }
 
     std::queue<std::vector<std::string>> q;
 
     csv::CSVReader reader(strFile, csv::DEFAULT_CSV);
     csv::CSVRow rows;
     for (size_t i = 0; reader.read_row(rows); i++) {
-        std::cout <<  rows["age"].get<uint64_t>();
-        std::cout <<  rows["name"].get<std::string>();
+        std::cout <<  rows["age"].get<uint64_t>() << " , " << rows["name"].get<std::string>() << std::endl;
 
         q.push({ std::to_string(rows["age"].get<uint64_t>()) + "|" + rows["name"].get<std::string>() });
     }
  
     std::ofstream out(strFile);
     auto writer = csv::make_csv_writer(out);
-
 
     for (; !q.empty(); q.pop())
         writer.write_row(q.front());
